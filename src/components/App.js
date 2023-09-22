787babe2fb76ebc12a686e4ec43bb86f97fd4897
thilaks-hash <thilagavathyr025@gmail.com>
2023-09-22 23:54:54 +0530

    updated tailwind

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      {" "}
+      <h1 className="text-3xl font-bold text-slate-900">Hello world!</h1>
+      <p className="bg-black text-red-400">heeee</p>
+    </div>
+  );
 }
 
 export default App;
